c9735fb730a2d9e7d49e6ae9252e7ffa092ac8fb
Tung Dinh <tungxd301@gmail.com>
2024-03-04 15:35:48 -0800

    Changes

--- src/main/java/com/tcloud/entity/Document.java
+++ src/main/java/com/tcloud/entity/Document.java
@@ -1,12 +1,16 @@
 package com.tcloud.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.extern.jackson.Jacksonized;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Data
@@ -17,9 +21,12 @@ public class Document {
     private byte[] entities;
     private byte[] states;
 
+    @JsonIgnore
+    private static ObjectMapper objectMapper;
+
     public static Document create() {
         try {
-            final ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper = new ObjectMapper();
             return Document.builder()
                     .documentId(UUID.randomUUID().toString())
                     .entities(objectMapper.writeValueAsBytes(new HashMap<>()))
@@ -29,5 +36,15 @@ public class Document {
             e.printStackTrace();
         }
         return Document.builder().build();
+    }
+
+    public void putEntity(@NonNull final String key, @NonNull final byte[] value) {
+        try {
+            final Map<String, byte[]> deserializedEntities =
+                    objectMapper.readValue(entities, new TypeReference<Map<String, byte[]>>() {});
+            deserializedEntities.put(key, value);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 }
