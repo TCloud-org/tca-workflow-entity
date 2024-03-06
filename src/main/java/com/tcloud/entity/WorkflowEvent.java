42b0c45a2b3298858e29548688d16bc216e8e785
Tung Dinh <tungxd301@gmail.com>
2024-03-05 20:27:07 -0800

    Changes

--- src/main/java/com/tcloud/entity/WorkflowEvent.java
+++ src/main/java/com/tcloud/entity/WorkflowEvent.java
@@ -1,7 +1,10 @@
 package com.tcloud.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 import java.time.ZonedDateTime;
@@ -9,10 +12,19 @@ import java.time.ZonedDateTime;
 @Data
 @Builder(toBuilder = true)
 @Jacksonized
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkflowEvent {
 
+    @JsonProperty("document")
     private Document document;
+
+    @JsonProperty("currentState")
     private String currentState;
+
+    @JsonProperty("nextState")
     private String nextState;
+
+    @JsonProperty("sentAt")
     private ZonedDateTime sentAt;
 }
