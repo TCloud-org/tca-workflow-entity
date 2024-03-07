0e5d1f7f64ce7375eb55501fed6f0801e4698f98
Tung Dinh <tungxd301@gmail.com>
2024-03-06 22:25:38 -0800

    Changes

---
+++ src/main/java/com/tcloud/dto/Route.java
@@ -0,0 +1,32 @@
+package com.tcloud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Jacksonized
+public class Route {
+    private Long routeId;
+
+    private String source;
+
+    private String service;
+
+    private String operation;
+
+    private String resultType;
+
+    private String resultName;
+
+    private Long graphId;
+
+    private ZonedDateTime createdAt;
+}
