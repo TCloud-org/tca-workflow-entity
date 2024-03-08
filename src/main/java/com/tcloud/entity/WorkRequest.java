0fa101af135d059088d7c6bc61997c8613d85f82
Tung Dinh <tungxd301@gmail.com>
2024-03-07 16:26:04 -0800

    Changes

--- src/main/java/com/tcloud/entity/WorkRequest.java
+++ src/main/java/com/tcloud/entity/WorkRequest.java
@@ -1,10 +1,16 @@
 package com.tcloud.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
-@Data
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder(toBuilder = true)
 @Jacksonized
 public class WorkRequest {
