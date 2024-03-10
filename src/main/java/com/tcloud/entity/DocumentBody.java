179282eb9aa4f5a48160452d0a7f12fc9e213ee2
Tung Dinh <tungxd301@gmail.com>
2024-03-10 15:16:30 -0700

    Changes

--- src/main/java/com/tcloud/entity/DocumentBody.java
+++ src/main/java/com/tcloud/entity/DocumentBody.java
@@ -1,10 +1,12 @@
 package com.tcloud.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.tcloud.dto.DocumentEntityChangeLog;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +14,7 @@ import java.util.Map;
 @Data
 @Builder(toBuilder = true)
 @Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DocumentBody {
     private Map<String, byte[]> entities;
     private List<DocumentEntityChangeLog> changeLogs;
@@ -19,6 +22,7 @@ public class DocumentBody {
     public static DocumentBody create() {
         return DocumentBody.builder()
                 .entities(new HashMap<>())
+                .changeLogs(new ArrayList<>())
                 .build();
     }
 }
