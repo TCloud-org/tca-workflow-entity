9c093efa821b25cc3fa91c37cd571b7d6116ab72
Tung Dinh <tungxd301@gmail.com>
2024-03-12 09:31:02 -0700

    Changes

--- src/main/java/com/tcloud/dto/XMLGraphState.java
+++ src/main/java/com/tcloud/dto/XMLGraphState.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Builder(toBuilder = true)
 @Data
@@ -15,5 +16,7 @@ public class XMLGraphState {
     private String service;
     private String operation;
     private Integer runningOrder;
+    private Integer duration;
+    private TimeUnit unit;
     private Map<String, XMLGraphStateResult> resultMap;
 }
