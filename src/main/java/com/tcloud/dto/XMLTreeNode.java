3df341d7c3224ba32154bc75ce57a69a96ee028c
Tung Dinh <tungxd301@gmail.com>
2024-03-12 16:51:46 -0700

    Changes

--- src/main/java/com/tcloud/dto/XMLTreeNode.java
+++ src/main/java/com/tcloud/dto/XMLTreeNode.java
@@ -3,6 +3,7 @@ package com.tcloud.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
@@ -15,4 +16,8 @@ public class XMLTreeNode {
     private String id;
     private String name;
     private List<XMLTreeNode> children;
+
+    public void addToChildren(@NonNull final XMLTreeNode node) {
+        children.add(node);
+    }
 }
