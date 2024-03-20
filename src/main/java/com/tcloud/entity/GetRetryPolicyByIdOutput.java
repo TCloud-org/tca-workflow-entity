7281e1fdace2724a83b53b6c3eafea8425a95462
Tung Dinh <tungxd301@gmail.com>
2024-03-19 21:07:30 -0700

    Changes

---
+++ src/main/java/com/tcloud/entity/GetRetryPolicyByIdOutput.java
@@ -0,0 +1,14 @@
+package com.tcloud.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder(toBuilder = true)
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GetRetryPolicyByIdOutput {
+    private RetryPolicy retryPolicy;
+}
