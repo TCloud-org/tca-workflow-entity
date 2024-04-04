7c972e4d2a89270d49cfd5691888e8c9a66b20ca
Tung Dinh <tungxd301@gmail.com>
2024-04-03 22:44:47 -0700

    Changes

--- src/main/java/com/tcloud/entity/WorkRequest.java
+++ src/main/java/com/tcloud/entity/WorkRequest.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.extern.jackson.Jacksonized;
 
 @Getter
@@ -13,6 +14,7 @@ import lombok.extern.jackson.Jacksonized;
 @AllArgsConstructor
 @Builder(toBuilder = true)
 @Jacksonized
+@ToString
 public class WorkRequest {
     private String workId;
     private Document document;
