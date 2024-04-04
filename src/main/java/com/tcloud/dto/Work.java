6756ff3ad56170be134bc0f83f958dd3e7d8c8aa
Tung Dinh <tungxd301@gmail.com>
2024-04-04 15:42:08 -0700

    Changes

---
+++ src/main/java/com/tcloud/dto/Work.java
@@ -0,0 +1,50 @@
+package com.tcloud.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Builder(toBuilder = true)
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Work {
+
+    private String workId;
+
+    private String clientId;
+
+    private String source;
+
+    private String service;
+
+    private String operation;
+
+    private String resultType;
+
+    private String resultName;
+
+    private Long graphId;
+
+    private Long workflowId;
+
+    private Long version;
+
+    private Integer runningOrder;
+
+    private Long executionTime;
+
+    private ZonedDateTime createdAt;
+
+    private ZonedDateTime updatedAt;
+
+    private String retryScheduleId;
+
+    private ZonedDateTime nextRetryAt;
+
+    private RouteMetadata metadata;
+}
+
