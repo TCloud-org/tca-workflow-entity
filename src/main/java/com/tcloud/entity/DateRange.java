ab74d4242418f62c9249ecf917ed29046b643cba
Tung Dinh <tungxd301@gmail.com>
2024-04-07 22:36:59 -0700

    Changes

--- src/main/java/com/tcloud/entity/DateRange.java
+++ src/main/java/com/tcloud/entity/DateRange.java
@@ -5,17 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
-import java.util.Map;
-
 @Data
 @Builder(toBuilder = true)
 @Jacksonized
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Clause {
-    private String attribute;
-    private String operator;
-    private String condition;
-    private Map<String, Boolean> checkbox;
-    private String input;
-    private DateRange date;
+public class DateRange {
+    private String start;
+    private String end;
 }
