895e11d94db364009bac169bf9bdcd42abe4b650
Tung Dinh <tungxd301@gmail.com>
2024-04-15 12:20:31 -0700

    Changes

---
+++ src/main/java/com/tcloud/dto/ApiWorkResult.java
@@ -0,0 +1,25 @@
+package com.tcloud.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApiWorkResult {
+    private Long workResultId;
+    private Long stateId;
+    private String workResultType;
+    private String workResultName;
+    private String workResultTarget;
+    private ZonedDateTime createdAt;
+}
