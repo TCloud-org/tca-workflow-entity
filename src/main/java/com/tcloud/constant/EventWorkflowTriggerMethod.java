c87eb10d49eb0dbd0651e53a76cc05e0c10c10cb
Tung Dinh <tungxd301@gmail.com>
2024-04-14 23:12:28 -0700

    Changes

--- src/main/java/com/tcloud/constant/EventWorkflowTriggerMethod.java
+++ src/main/java/com/tcloud/constant/EventWorkflowTriggerMethod.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum EventWorkflowTriggerMethod {
-    CODE("code"),
+    API("api"),
     WEBHOOK("webhook");
 
     private final String value;
