309a2c9db2b8a29cc6c41aa3a94196b8792493f5
Tung Dinh <tungxd301@gmail.com>
2024-04-15 09:48:00 -0700

    Changes

---
+++ src/main/java/com/tcloud/dto/ApiWorkflowState.java
@@ -0,0 +1,29 @@
+package com.tcloud.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApiWorkflowState {
+    private Long stateId;
+    private String stateType;
+    private String source;
+    private String service;
+    private String operation;
+    private Integer runningOrder;
+    private Long retryPolicyId;
+    private Integer duration;
+    private String unit;
+    private String workResultType;
+    private String workResultName;
+    private String workResultTarget;
+}
