9edf2851aa98fe41762f94446a597c48d9d01338
Tung Dinh <tungxd301@gmail.com>
2024-05-26 15:29:04 -0700

    Changes

--- src/main/java/com/tcloud/dto/GraphArch.java
+++ src/main/java/com/tcloud/dto/GraphArch.java
@@ -7,8 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
-import java.util.List;
-
 @Data
 @Builder(toBuilder = true)
 @NoArgsConstructor
@@ -16,6 +14,6 @@ import java.util.List;
 @Jacksonized
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GraphArch {
-    Long retryPolicyId;
-    List<UIBuilderGraphState> states;
+    XMLGraphFormat xmlGraphFormat;
+    UIBuilderGraphFormat uiBuilderGraphFormat;
 }
